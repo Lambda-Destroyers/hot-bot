8983a7b80c839c3250cb612062c97bbb9f0e5147
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-07-12 10:02:40 -0700

    deleted the modcopy folder and all GPTtestXX.py function properly

--- modules/GPTtest60.py
+++ modules/GPTtest60.py
@@ -18,6 +18,10 @@ def get_current_price(product_id):
     data = response.json()
     return data['data']['amount']
 
+# Read the API key from gpt_key.txt file
+with open('gpt_api.txt', 'r') as file:
+    gpt_api = file.read().strip()
+
 # Retrieve current prices for BTC-USD and ETH-USD
 btc_usd_price = get_current_price("BTC-USD")
 eth_usd_price = get_current_price("ETH-USD")
@@ -31,7 +35,7 @@ api_url = "https://api.pro.coinbase.com/products"
 
 # Calculate the start and end dates
 end_date = datetime.datetime.now()
-start_date = end_date - datetime.timedelta(days=90)
+start_date = end_date - datetime.timedelta(days=60)
 
 # Function to retrieve historical data for a given product
 def get_historical_data(product_id):
@@ -68,7 +72,7 @@ ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 ax1.xaxis.set_major_locator(mdates.MonthLocator())
 ax1.plot(btc_timestamps, btc_closes, color='blue')
 ax1.vlines(btc_timestamps, btc_lows, btc_highs, color='black', linewidth=1)
-ax1.set_title("Bitcoin (BTC-USD) - 90 Day Historical Data")
+ax1.set_title("Bitcoin (BTC-USD) - 60 Day Historical Data")
 ax1.set_xlabel("Date")
 ax1.set_ylabel("Price (USD)")
 ax1.tick_params(axis='x', rotation=45)
@@ -77,10 +81,50 @@ ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 ax2.xaxis.set_major_locator(mdates.MonthLocator())
 ax2.plot(eth_timestamps, eth_closes, color='green')
 ax2.vlines(eth_timestamps, eth_lows, eth_highs, color='black', linewidth=1)
-ax2.set_title("Ethereum (ETH-USD) - 90 Day Historical Data")
+ax2.set_title("Ethereum (ETH-USD) - 60 Day Historical Data")
 ax2.set_xlabel("Date")
 ax2.set_ylabel("Price (USD)")
 ax2.tick_params(axis='x', rotation=45)
 
 plt.tight_layout()
 plt.show()
+
+def get_investment_recommendation(btc_data, eth_data):
+    url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {gpt_api}",
+    }
+    btc_price = btc_data[0][4]
+    eth_price = eth_data[0][4]
+    btc_prompt = f"\n\nBTC-USD historical data:\n- Opens: {btc_opens}\n- Highs: {btc_highs}\n- Lows: {btc_lows}\n- Closes: {btc_closes}"
+    eth_prompt = f"\n\nETH-USD historical data:\n- Opens: {eth_opens}\n- Highs: {eth_highs}\n- Lows: {eth_lows}\n- Closes: {eth_closes}"
+    prompt = f"Analyze the pricing data from the Coinbase API call. Recommend an investment strategy with 8% ROI.{btc_prompt}{eth_prompt}\n\nBTC-USD price: {btc_price}\nETH-USD price: {eth_price}"
+    body = {
+        "prompt": prompt,
+        "max_tokens": 585,
+        "temperature": 0.1
+    }
+    print("prompt: ", prompt)
+    response = requests.post(url, headers=headers, json=body, timeout=120)
+    print("response: ", response)
+    result = response.json()
+    print("result: ", result)
+    choices = result.get('choices', [])  # Get the value of 'choices', or an empty list if it doesn't exist
+    if choices:
+        completion = choices[0].get('text', '').strip()
+        return completion
+    else:
+        return ""
+
+# Create data dictionary
+data = {
+    "btc_data": btc_data,
+    "eth_data": eth_data,
+}
+
+# Get investment recommendation
+investment_recommendation = get_investment_recommendation(data["btc_data"], data["eth_data"])
+
+# Print investment recommendation
+print("Investment Recommendation:", investment_recommendation)
