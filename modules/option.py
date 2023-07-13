25a3fd03208eabcdd10f67d5160557286d386393
Slava Makeev <unclearnuclear@yahoo.com>
2023-07-13 23:09:54 +0000

    Reduced to 1 file

--- modules/option.py
+++ modules/option.py
@@ -3,9 +3,9 @@ console = Console()
 from modules.investment_recommendation import get_investment_recommendation
 
 
-def user_option(btc_data, eth_data, btc_opens, btc_highs, btc_lows, btc_closes, eth_opens, eth_highs, eth_lows, eth_closes, gpt_api, option):
+def user_option(btc_data, eth_data, btc_opens, btc_highs, btc_lows, btc_closes, eth_opens, eth_highs, eth_lows, eth_closes, gpt_api, days, option):
     investment_recommendation = get_investment_recommendation(btc_data, eth_data, btc_opens, btc_highs, btc_lows, btc_closes, eth_opens, eth_highs, eth_lows, eth_closes, gpt_api, option)
 
     # Print investment recommendation
     print("Investment Recommendation:", investment_recommendation)
-    
+    return
