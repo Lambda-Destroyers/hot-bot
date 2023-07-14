d681a610fbd90197da0bc7361daefd5341122c78
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-07-14 14:01:47 -0400

    Ascii Art

--- menu.py
+++ menu.py
@@ -3,6 +3,8 @@ import sys
 from rich.console import Console
 from rich.prompt import Prompt
 import importlib
+from rich.progress import Progress
+import time
 
 console = Console()
 
@@ -14,7 +16,7 @@ def menu():
             "\n1. [bold green]Help[/bold green]\n2. [bold green]AI Analysis[/bold green]\n3. [bold medium_violet_red]Live trading: [/bold medium_violet_red](In production) \n4. [bold green]About us[/bold green] \n5. [bold red]Exit[/bold red]",
             soft_wrap=False,
         )
-        choice = Prompt.ask('Choose a task (Enter the number)', choices=["1", "2", "3", '4', '5'], default='5')
+        choice = Prompt.ask('\nChoose a task: (Enter the number)', choices=["1", "2", "3", '4', '5'], default='5')
 
         if choice == '1':
             help_menu()
@@ -40,17 +42,43 @@ def help_menu():
     console.clear()
     console.print('This will offer an overview of the program')
     console.print('Press Enter to go back to the main menu...')
+    crypto_bot = Prompt.ask('Choose [bold purple]info, commands[/bold purple] (Enter the option number)', choices=["info", "commands"], default='1')
+
+    if crypto_bot == "info":
+    # Provide information about the crypto bot
+      print("Crypto bot information:")
+      print("- This bot helps you with cryptocurrency-related tasks.")
+      print("- It can provide information about various cryptocurrencies, market trends, and more.")
+      print("- You can also use it to execute specific commands for trading or managing your cryptocurrencies.")
+    
+    elif crypto_bot == "commands":
+      # Execute commands for the crypto bot
+      console.print("Generating response...")
+      print("If you need help ")
+      console.print('press enter to exit')
+      # Add your code here to handle commands
+    else:
+      print("Invalid option selected.")
+    console.print('\nPress "enter" to [red]exit[/red]')
     input()
 
-def about_us():
-    console.print('Andrew Carroll: Andrew Carroll is a man')
-    console.print('Slava Makeev: Slave Makeeve was born...')
-    console.print('Jared Ciccarello: Woke up drunk', style='bold')
-    input()
+
+
+# def about_us():
+#     console.print('\nAndrew Carroll: Andrew Carroll is a man')
+#     \
+#     console.print('\nSlava Makeev: Slave Makeeve was born...')
+
+#     console.print('\nJared Ciccarello: Woke up drunk', style='bold')
+    
+#     console.print('\nPress "enter" to [red]exit[/red]')
+#     input()
+
+
 
 def choice2():
-    day_choice = Prompt.ask('Choose [bold purple]30, 60, or 90 days[/bold purple] (Enter the number)', choices=["30", "60", "90"], default='30')
-    option_choice = Prompt.ask('Choose a strategy Conservative (C), Risky (R), Beginner (B)', choices=["Conservative", "Risky", "Beginner", "C", "R", "B"], default='4')
+    day_choice = Prompt.ask('\nChoose a dataset of [bold purple]30, 60, or 90 days[/bold purple]. (Enter the number)', choices=["30", "60", "90"], default='30')
+    option_choice = Prompt.ask('\nChoose a strategy Conservative (C), Risky (R), Beginner (B)', choices=["Conservative", "Risky", "Beginner", "C", "R", "B"], default='4')
     
     if option_choice.lower() == "Conservative" or option_choice.lower() == "c":
         option_choice_validated = "conservative"
@@ -63,7 +91,14 @@ def choice2():
     else:
         exit    
 
-    console.print("Generating response...")
+
+    with Progress(console=console, transient=True) as progress:
+        task = progress.add_task("[cyan]Loading...", total=10)
+        for _ in range(10):
+            progress.update(task, advance=1)
+            time.sleep(0.2)  # Add a delay to simulate processing
+        progress.stop()
+    console.clear()
 
     user_input(day_choice, option_choice_validated)
     
@@ -77,9 +112,118 @@ def user_input(days, option):
     else:
         exit
     
+    
 def end_programm():
-    input(console.print('Press "enter" to [red]exit[/red]'))
+    console.print('\nPress "enter" to [red]exit[/red]')
+    input()
     exit
 
-if __name__ == '__main__':        
-    menu()
+def about_us():
+    while True:
+        console.clear()
+        console.print("[bold cyan]The HotBot Team[/bold cyan]")
+        console.print("Select a team member to learn more:")
+        console.print("1. [bold yellow]Andrew Carroll[/bold yellow]")
+        console.print("2. [bold yellow]Slava Makeev[/bold yellow]")
+        console.print("3. [bold yellow]Jared Cicarrello[/bold yellow]")
+        console.print("4. [bold red]Back to main menu[/bold red]")
+
+        choice = Prompt.ask('Choose a team member (Enter the number)', choices=["1", "2", "3", "4"], default='4')
+
+        if choice == '1':
+            show_bio("Andrew Carroll", "Storytelling, Magic, Software Dev", '''
+                __
+               / _\ #
+               \c /  #
+               / \___ #
+               \`----`#==>  
+               |  \  #
+    ,%.-"""---'`--'\#_
+   %%/             |__`|
+  .%'\     |   \   /  //
+  ,%' >   .'----\ |  
+     < <<`       ||
+      `\\\       ||
+        )\\      )\\
+\n^^^^^"""^^^^^^""^^^^^^^^^^
+                     ''', "https://github.com/iamandrewcarroll")
+
+        elif choice == '2':
+            show_bio("Slava Makeev", "Outdoors, Adventure, Software Dev", """
+     ; 
+     ;;
+     ;';.
+     ;  ;;
+     ;   ;;
+     ;    ;;
+     ;    ;;
+     ;   ;'
+     ;  ' 
+,;;;,; 
+;;;;;;
+`;;;;'     """, "https://github.com/S-Makeev")
+
+        elif choice == '3':
+            show_bio("Jared Cicarrello", "Software Dev, Marine, Civil Service", """
+                          \ : /
+                        '-: __ :-'
+                        -:  _ :--
+                          ,==.
+                          \\\//
+                        .-~~-.
+                      ,",-""-.".
+                      | |      | |
+                      | |      | |
+                      ". `,",-" ,'
+                        `| |_,-' 
+                      -' |   | '-
+            ,sSSSSs,    (   )
+            sS';:'`Ss   )  (  
+           ; ~ <  ~  /  (  )
+            S, ''  SJ (  ;/
+            sL_~~_;(S_)  _7
+/\        'J)_.-' />'-' `Z
+\ \         /-;-A'-'|'--'-j\\
+ \ \        )  |/   :    /  \\
+  \ \       | | |    '._.'|  L
+   \ \      | | |       | \  J
+    \ \    _/ | |       |  ',|
+     \ L.,' | | |       |   |/
+    _;-r-<_.| \=\    __.;  _/
+      {_}"  L-'  '--'   / /|
+            |   ,      |  \|
+            |   |      |   ")
+            L   ;|     |   /|
+           /|    ;     |  / |
+          | |    ;     |  ) |
+         |  |    ;|    | /  |
+         | ;|    ||    | |  |
+         L-'|____||    )/   |
+             % %/ '-,- /    /
+             |% |   \%/_    |
+          ___%  (   )% |'-; |
+        C;.---..'   >%,(   "'
+                   /%% /
+                  Cccc' """, "https://github.com/JaredCiccarello")
+
+        elif choice == '4':
+            break
+
+        else:
+            console.print('Invalid choice. Please try again.')
+
+def show_bio(name, interests, avatar, github_link):
+    console.clear()
+    console.print(f"[bold yellow]{name}[/bold yellow]")
+    console.print(f"Heart: {interests}")
+    console.print("Avatar:")
+    console.print(avatar)
+    console.print(f"GitHub: [link={github_link}]{github_link}[/link]")
+    console.print('Press Enter to go back to the team members...')
+    input()
+
+if __name__ == '__main__':
+    try:
+        menu()
+    except KeyboardInterrupt:
+        console.print('\nCtrl+C detected. Exiting...')
